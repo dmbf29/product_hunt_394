b002674a93b0d7e52c059fd57bcf10efac7a4da8
Douglas Berkley <douglasmberkley@gmail.com>
2020-05-29 10:00:50 +0900

    added system and unit tests. along with Travis config

---
+++ test/system/products_test.rb
@@ -0,0 +1,11 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit '/'
+    #
+    # save_and_open_screenshot
+    assert_selector "h1", text: "Awesome Products"
+    assert_selector ".card-product", count: Product.count
+  end
+end
